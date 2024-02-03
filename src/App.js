96287c8faae68cc2d347b04f67e9150dd3dc16c9
cxnvti <jenishbekov24bj@gmail.com>
2024-02-03 13:31:21 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo, useCallback } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const OrderDetails = ({ products }) => {
+  const [quantities, setQuantities] = useState({});
+
+  const totalCost = useMemo(() => {
+    console.log('Вычисление общей стоимости заказа');
+
+    return Object.keys(quantities).reduce((total, productId) => {
+      const product = products.find(p => p.id === parseInt(productId));
+      const quantity = quantities[productId];
+
+      if (product) {
+        return total + product.price * quantity;
+      }
+      return total;
+    }, 0);
+
+  }, [quantities, products]);
+
+  const handleQuantityChange = useCallback((productId, quantity) => {
+    setQuantities(prevQuantities => ({
+      ...prevQuantities,
+      [productId]: quantity
+    }));
+  }, []);
+
+    return (
+      <div style={{ fontFamily: 'Arial, sans-serif', maxWidth: '400px', margin: 'auto', background: '#283142', color: '#fff', padding: '20px' }}>
+      <h2>Детали заказа</h2>
+      <ul style={{ listStyleType: 'none', padding: 0 }}>
+        {products.map(product => (
+          <li key={product.id} style={{ marginBottom: '10px' }}>
+            <span>{product.name}:</span>
+            <input
+              type="number"
+              value={quantities[product.id] || 0}
+              onChange={(e) => handleQuantityChange(product.id, parseInt(e.target.value))}
+              style={{ marginLeft: '10px', width: '50px' }}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <p style={{ marginTop: '20px', fontSize: '18px' }}>Общая стоимость заказа: ${totalCost}</p>
     </div>
   );
-}
+};
+
+const App = () => {
+  const initialProducts = [
+    { id: 1, name: 'Картошка (10$)', price: 10 },
+    { id: 2, name: 'Помидоры (20$)', price: 20 },
+    { id: 3, name: 'Огурцы (30$)', price: 30 },
+  ];
+
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '50px', background: '#1a1f26', minHeight: '100vh' }}>
+      <OrderDetails products={initialProducts} />
+    </div>
+  );
+};
 
 export default App;
